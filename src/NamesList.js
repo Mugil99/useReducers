ed1b347ec1d704808dbc78dacaa6023d29abb6d0
Mugil99 <mugilya99@gmail.com>
2023-05-26 21:50:15 +0530

    first commit

---
+++ src/NamesList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NamesList = ({ names }) => {
+  return (
+    <ul>
+      {names.map((name, index) => (
+        <li key={index}>{name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default NamesList;
